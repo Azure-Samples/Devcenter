71469e2bb59923223bd9d4c10b5b63381b973d63
Oliver Gulich <70239916+oliverlabs@users.noreply.github.com>
2023-06-20 22:47:23 +0100

    Oliverlabs/feature winget (#4)
    * added winget install files 
    
    added winget install files to replace choco
    
    * further improvements for automation
    
    included the following:
    - a new script to install the pre-release version of winget
    - corrected some package names that did not exist in winget
    - added logic to update settings.json configuration file for winget to enable the experimental winget configuration feature
    - added --accept-source-agreements to avoid interactive confirmations
    
    * further updates
    
    further updates
    
    * removed redundant lines from the script
    
    removed redundant lines from the script

--- imageBuilderScripts/experimental-dsc-kubeTools.ps1
+++ imageBuilderScripts/experimental-dsc-kubeTools.ps1
@@ -3,4 +3,36 @@
 # Refer to the following Microsoft Learn page to find out how to configure winget experimental 
 # features: https://learn.microsoft.com/en-us/windows/package-manager/configuration/#enable-the-winget-configuration-experimental-configuration-preview-feature
 
-winget configure .\kube-tools.dsc.yaml
+# Install a pre-release version of Winget
+$progressPreference = 'silentlyContinue'
+$latestWingetMsixBundleUri = $(Invoke-RestMethod https://github.com/microsoft/winget-cli/releases/1.6.1573-preview).assets.browser_download_url | Where-Object { $_.EndsWith(".msixbundle") }
+
+Write-Information "Installing winget..."
+$progressPreference = 'silentlyContinue'
+$latestWingetMsixBundleUri = "https://github.com/microsoft/winget-cli/releases/download/v1.6.1573-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+$latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
+Write-Information "Downloading winget to artifacts directory..."
+Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+#Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage $latestWingetMsixBundle
+
+# Enable experimental features in winget settings file (settings.json)
+
+$path = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
+$content = @"
+{
+    "`$schema": "https://aka.ms/winget-settings.schema.json",
+
+    // For documentation on these settings, see: https://aka.ms/winget-settings
+    // "source": {
+    //    "autoUpdateIntervalInMinutes": 5
+    // },
+	"experimentalFeatures": {
+       "configuration": true
+   }
+}
+"@
+$content | Set-Content $path
+
+winget configure -f .\kube-tools.dsc.yaml --accept-configuration-agreements
--- imageBuilderScripts/winget-vscode.ps1
+++ imageBuilderScripts/winget-vscode.ps1
@@ -1,16 +1,34 @@
-# Check if winget is installed
-if (-not(Get-Command winget -ErrorAction SilentlyContinue)) {
-    # Install winget
-    Write-Information "Installing winget..."
-    $progressPreference = 'silentlyContinue'
-    $latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object { $_.EndsWith(".msixbundle") }
-    $latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
-    Write-Information "Downloading winget to artifacts directory..."
-    Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
-    Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-    Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-    Add-AppxPackage $latestWingetMsixBundle
+# Install a pre-release version of Winget
+$progressPreference = 'silentlyContinue'
+$latestWingetMsixBundleUri = $(Invoke-RestMethod https://github.com/microsoft/winget-cli/releases/1.6.1573-preview).assets.browser_download_url | Where-Object { $_.EndsWith(".msixbundle") }
+
+Write-Information "Installing winget..."
+$progressPreference = 'silentlyContinue'
+$latestWingetMsixBundleUri = "https://github.com/microsoft/winget-cli/releases/download/v1.6.1573-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+$latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
+Write-Information "Downloading winget to artifacts directory..."
+Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+#Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage $latestWingetMsixBundle
+
+# Enable experimental features in winget settings file (settings.json)
+
+$path = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
+$content = @"
+{
+    "`$schema": "https://aka.ms/winget-settings.schema.json",
+
+    // For documentation on these settings, see: https://aka.ms/winget-settings
+    // "source": {
+    //    "autoUpdateIntervalInMinutes": 5
+    // },
+	"experimentalFeatures": {
+       "configuration": true
+   }
 }
+"@
+$content | Set-Content $path
 
 # Install VS Code with winget
-winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Microsoft.VisualStudioCode --accept-source-agreements
