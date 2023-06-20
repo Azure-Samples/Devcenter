a43edd206f86a05d9023489425849fb1b4bf1f84
Oliver Gulich <70239916+oliverlabs@users.noreply.github.com>
2023-06-20 19:52:07 +0100

    further improvements for automation
    included the following:
    - a new script to install the pre-release version of winget
    - corrected some package names that did not exist in winget
    - added logic to update settings.json configuration file for winget to enable the experimental winget configuration feature
    - added --accept-source-agreements to avoid interactive confirmations

--- imageBuilderScripts/winget-azure.ps1
+++ imageBuilderScripts/winget-azure.ps1
@@ -1,38 +1,62 @@
-# Check if winget is installed
-if (-not(Get-Command winget -ErrorAction SilentlyContinue)) {
-    # Install winget
-    Write-Information "Installing winget..."
-    $progressPreference = 'silentlyContinue'
-    $latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object { $_.EndsWith(".msixbundle") }
-    $latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
-    Write-Information "Downloading winget to artifacts directory..."
-    Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
-    Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-    Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-    Add-AppxPackage $latestWingetMsixBundle
+# Install a pre-release version of Winget
+$progressPreference = 'silentlyContinue'
+$latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/Pre-release).assets.browser_download_url | Where-Object { $_.EndsWith(".msixbundle") }
+$latestWingetMsixBundleUri = $(Invoke-RestMethod https://github.com/microsoft/winget-cli/releases/1.6.1573-preview).assets.browser_download_url | Where-Object { $_.EndsWith(".msixbundle") }
+
+$latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
+Write-Information "Downloading winget to artifacts directory..."
+Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+#Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage $latestWingetMsixBundle
+
+Write-Information "Installing winget..."
+$progressPreference = 'silentlyContinue'
+$latestWingetMsixBundleUri = "https://github.com/microsoft/winget-cli/releases/download/v1.6.1573-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+$latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
+Write-Information "Downloading winget to artifacts directory..."
+Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+#Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage $latestWingetMsixBundle
+
+# Enable experimental features in winget settings file (settings.json)
+
+$path = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
+$content = @"
+{
+    "`$schema": "https://aka.ms/winget-settings.schema.json",
+
+    // For documentation on these settings, see: https://aka.ms/winget-settings
+    // "source": {
+    //    "autoUpdateIntervalInMinutes": 5
+    // },
+	"experimentalFeatures": {
+       "configuration": true
+   }
 }
+"@
+$content | Set-Content $path
 
 # Install apps
 
-winget install --id 7zip.7zip -e
-winget install --id Adobe.AdobeAcrobatReaderDC -e
-winget install --id Microsoft.AzCopy -e
-winget install --id Microsoft.AzureCLI -e
-winget install --id Microsoft.AzureIoTInstaller -e
-winget install --id Docker.DockerDesktop -e
-winget install --id Mozilla.Firefox -e
-winget install --id Flux.Flux -e
-winget install --id GitHub.cli -e
-winget install --id Git.Git -e
-winget install --id GitHub.GitHubDesktop -e
-winget install --id Google.Chrome -e
-winget install --id stedolan.jq -e
-winget install --id Kubernetes.kubectl -e
-winget install --id Kubernetes.Helm -e
-winget install --id dotPDNLLC.Paint.NET -e
-winget install --id Pulumi.Pulumi -e
-winget install --id Microsoft.PowerShell -e
-winget install --id ServiceBusExplorer.ServiceBusExplorer -e
-winget install --id HashiCorp.Terraform -e
-winget install --id Microsoft.VisualStudioCode -e
-winget install --id Git.Git -e
+winget install --id 7zip.7zip -e --accept-source-agreements 
+winget install --id Adobe.Acrobat.Reader.64-bit -e --accept-source-agreements 
+winget install --id Microsoft.Azure.AZCopy.10 -e --accept-source-agreements
+winget install --id Microsoft.AzureCLI -e --accept-source-agreements
+winget install --id Docker.DockerDesktop -e --accept-source-agreements
+winget install --id Mozilla.Firefox -e --accept-source-agreements
+winget install --id GitHub.cli -e --accept-source-agreements
+winget install --id Git.Git -e --accept-source-agreements
+winget install --id GitHub.GitHubDesktop -e --accept-source-agreements
+winget install --id Google.Chrome -e --accept-source-agreements
+winget install --id stedolan.jq -e --accept-source-agreements
+winget install --id Kubernetes.kubectl -e --accept-source-agreements
+winget install --id Kubernetes.Helm -e --accept-source-agreements
+winget install --id dotPDNLLC.Paint.NET -e --accept-source-agreements
+winget install --id Pulumi.Pulumi -e --accept-source-agreements
+winget install --id Microsoft.PowerShell -e --accept-source-agreements
+winget install --id ServiceBusExplorer.ServiceBusExplorer -e --accept-source-agreements
+winget install --id HashiCorp.Terraform -e --accept-source-agreements
+winget install --id Microsoft.VisualStudioCode -e --accept-source-agreements
+winget install --id Microsoft.Azure.IoTExplorer -e --accept-source-agreements
--- imageBuilderScripts/winget-kube.ps1
+++ imageBuilderScripts/winget-kube.ps1
@@ -1,23 +1,37 @@
-# Check if winget is installed
-if (-not(Get-Command winget -ErrorAction SilentlyContinue)) {
-    # Install winget
-    Write-Information "Installing winget..."
-    $progressPreference = 'silentlyContinue'
-    $latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object { $_.EndsWith(".msixbundle") }
-    $latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
-    Write-Information "Downloading winget to artifacts directory..."
-    Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
-    Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-    Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-    Add-AppxPackage $latestWingetMsixBundle
+Write-Information "Installing winget..."
+$progressPreference = 'silentlyContinue'
+$latestWingetMsixBundleUri = "https://github.com/microsoft/winget-cli/releases/download/v1.6.1573-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+$latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
+Write-Information "Downloading winget to artifacts directory..."
+Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+#Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage $latestWingetMsixBundle
+
+# Enable experimental features in winget settings file (settings.json)
+
+$path = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
+$content = @"
+{
+    "`$schema": "https://aka.ms/winget-settings.schema.json",
+
+    // For documentation on these settings, see: https://aka.ms/winget-settings
+    // "source": {
+    //    "autoUpdateIntervalInMinutes": 5
+    // },
+	"experimentalFeatures": {
+       "configuration": true
+   }
 }
+"@
+$content | Set-Content $path
 
 # Install apps
 
-winget install --id Microsoft.AzCopy -e
-winget install --id Microsoft.VisualStudioCode -e
-winget install --id Microsoft.PowerShell -e
-winget install --id Microsoft.AzureCLI -e
-winget install --id Kubernetes.kubectl -e
-winget install --id Git.Git -e
-winget install --id Kubernetes.Helm -e
+winget install --id Microsoft.AzCopy -e --accept-source-agreements
+winget install --id Microsoft.VisualStudioCode -e --accept-source-agreements
+winget install --id Microsoft.PowerShell -e --accept-source-agreements
+winget install --id Microsoft.AzureCLI -e --accept-source-agreements
+winget install --id Kubernetes.kubectl -e --accept-source-agreements
+winget install --id Git.Git -e --accept-source-agreements
+winget install --id Kubernetes.Helm -e --accept-source-agreements
